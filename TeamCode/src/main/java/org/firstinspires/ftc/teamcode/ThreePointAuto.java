b6b581d1d1ca7f758b92a71dc49630e53e81265b
SNiranjanSS <saisainiranjan@gmail.com>
2025-09-07 14:16:59 -0500

    Added 3 point auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreePointAuto.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="3ptAuto")
+public class ThreePointAuto extends LinearOpMode {
+    private MecanumWheels MecanumWheels;
+    private ElapsedTime timer = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+        MecanumWheels = new MecanumWheels(this);
+
+        waitForStart();
+
+        timer.reset();
+        while(timer.time()<=2){
+            MecanumWheels.move(0, 1, 0);
+        }
+    }
+}
