f104574c8144c984d6faa9cb9e4d4694cf2eef69
SNiranjanSS <saisainiranjan@gmail.com>
2025-11-15 14:25:25 -0600

    Create Auto_Back.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Back.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="Back Auton")
+public class Auto_Back extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        /**/
+    }
+}
