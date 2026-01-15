55b156930e201f1b3547c4cc5bf8433d35806ef4
radhika0816 <radhikagpt0816@gmail.com>
2026-01-14 18:44:53 -0600

    Teleop test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mecanum_test.java
@@ -0,0 +1,159 @@
+package org.firstinspires.ftc.teamcode;
+
+//region -- Imports ---
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.utils.RisingEdge;
+//endregion
+
+//region --- Controls ---
+//----------------------------------------------------------------------
+// Joystick 1 -----------------------------------------------------------
+//  - Left Stick        - Mecanum Drive
+//  - Right Stick       - Mecanum Rotate
+//  - Left Stick Click  - Drive Speed High/Low
+//  - Right Stick Click - Rotate Speed High/Low
+//
+//  - Dpad Up           - Move Forward (Slow)
+//  - Dpad Down         - Move Back (Slow)
+//  - Dpad Right        - Move Right (Slow)
+//  - Dpad Left         - Move Left (Slow)
+//
+//  - Right Trigger     - Intake Spin In
+//  - Right Bumpers     - Intake Spin Out
+//  - Left Trigger      - Intake Arm Out
+//  - Left Bumpers      - Intake Arm Back
+//
+//  - Y (▲)             - Next Step in Current Mode
+//  - A (✕)             - Previous Step in Current Mode
+//  - X (■)             - Intake In/Out
+//  - B (○)             -
+//
+//----------------------------------------------------------------------
+// Joystick 2 -----------------------------------------------------------
+//  - Left Stick        -
+//  - Right Stick       -
+//  - Left Stick Click  - Reset Lift Encoder
+//  - Right Stick Click - Reset Intake Encoder
+//
+//  - Dpad Up           - Manual Lift Up
+//  - Dpad Down         - Manual Lift Down
+//  - Dpad Right        - Manual Intake Out
+//  - Dpad Left         - Manual Intake In
+//
+//  - Right Trigger     -
+//  - Right Bumpers     -
+//  - Left Trigger      -
+//  - Left Bumpers      -
+
+//  - Y (▲)             - Mode -> High Basket
+//  - A (✕)             - Mode -> Low Basket
+//  - X (■)             - Mode -> Climbing
+//  - B (○)             - Mode -> Specimens
+//----------------------------------------------------------------------
+//endregion
+
+@TeleOp(name="TeleOpOld", group="1")
+public class TeleOp_Mecanum_test extends LinearOpMode
+{
+    //------------------------------------------------------------------------------------------
+    // Variables
+    //------------------------------------------------------------------------------------------
+    RobotHardware _robot = new RobotHardware(this);
+    public ElapsedTime _runtime = new ElapsedTime();
+
+
+    enum position{
+        Close,
+        Medium,
+        Far,
+        None
+    }
+
+
+
+
+
+    //------------------------------------------------------------------------------------------
+    //--- OpMode
+    //------------------------------------------------------------------------------------------
+    @Override
+    public void runOpMode()
+    {
+        //------------------------------------------------------------------------------------------
+        //--- Robot Initialize
+        //------------------------------------------------------------------------------------------
+        int robotVersion = 1; //--- 1 for CRAB-IER and 2 for ARIEL
+        int shooterSpeedRpm = 0;
+        boolean isThreeBallMode = false;
+        position robotPosition = position.None;
+
+        _robot.init(robotVersion);
+        RisingEdge g1RE = new RisingEdge();
+
+        //------------------------------------------------------------------------------------------
+        //--- Display and wait for the game to start (driver presses START)
+        //------------------------------------------------------------------------------------------
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        _runtime.reset();
+
+
+        //------------------------------------------------------------------------------------------
+        //--- Hardware Initialize
+        //------------------------------------------------------------------------------------------
+        _robot.shooter.initialize();
+       // _robot.intake.initialize();
+        _robot.lights.initialize();
+
+        //------------------------------------------------------------------------------------------
+        //--- Run until the end of the match (driver presses STOP)
+        //------------------------------------------------------------------------------------------
+        while (opModeIsActive()) {
+
+            //------------------------------------------------------------------------------------------
+            //--- Hardware Run (updates lights, etc.)
+            //------------------------------------------------------------------------------------------
+
+            //------------------------------------------------------------------------------------------
+            //--- Start Telemetry Display
+            //------------------------------------------------------------------------------------------
+            telemetry.addData("Status", "Run Time: " + _runtime.toString());
+
+            //------------------------------------------------------------------------------------------
+            //--- Drive
+            //------------------------------------------------------------------------------------------
+           // _robot.drive.driveControl(0.5); //--- Both D-pad for directional movement and Joysticks for mecanum movement
+            _robot.driveRR.driveControl(1);
+            _robot.limelightHardware2Axis.loop();
+
+            _robot.driveRR.mecanumDrive.updatePoseEstimate();
+
+            Pose2d pose = _robot.driveRR.getPosition();
+            telemetry.addData("x", pose.position.x);
+            telemetry.addData("y", pose.position.y);
+            telemetry.addData("heading", pose.heading);
+            telemetry.update();
+
+
+
+            //------------------------------------------------------------------------------------------
+            //--- Intake
+            //------------------------------------------------------------------------------------------
+           // _robot.intake.testColorSensors();  //--- Show color sensor values for tuning
+
+
+            //------------------------------------------------------------------------------------------
+            //--- Update Telemetry Display
+            //------------------------------------------------------------------------------------------
+            telemetry.update();
+
+
+        }
+    }
+}
